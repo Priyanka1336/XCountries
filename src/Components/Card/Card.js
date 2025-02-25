78d62b567af3976d0f268cfd366e8693c10d177c
Dikshit Kaushal <dikshitkaushal49@.com>
2025-02-26 03:45:22 +0530

    Added Search functionality1

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,7 @@ export default function OutlinedCard({ data }) {
   return (
     <div className="countryCard">
       <img src={data.png} alt={data.png} className="image" />
-      <h3>{data.common}</h3>
+      <h2>{data.common}</h2>
     </div>
   );
 }
